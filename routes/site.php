2261c7c508989c730aa42103d733c72ff38c45cc
Henni Karam <karam.tenes@gmail.com>
2018-11-02 11:09:44 +0100

    Replace web route by site

--- routes/site.php
+++ routes/site.php
@@ -2,15 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Site Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register Site routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "site" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    
 });
