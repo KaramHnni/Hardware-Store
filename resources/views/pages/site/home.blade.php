e07c2e8b1b4ba81acefacc4d8e197a44fd531de0
Henni Karam <karam.tenes@gmail.com>
2018-11-02 17:11:41 +0100

    migrating users and password resets tables

--- resources/views/pages/site/home.blade.php
+++ resources/views/pages/site/home.blade.php
@@ -3,7 +3,7 @@
     {{env('APP_NAME')}}
 @endsection
 @section('main-content')
-<div class="min-h-screen">
+<div class="bg-green-lightest w-full min-h-screen">
 @include('components.site.main-header')
     
         <div class=" w-1/2 text-center  mx-auto my-32">
