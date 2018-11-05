942e2af92fa956737c6c1838176432b6ff644304
Henni Karam <karam.tenes@gmail.com>
2018-11-05 18:48:14 +0100

    Add Blog Index Controller

---
+++ app/Http/Controllers/Blog/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function show(){
+        return view('pages.blog.index');
+    }
+}
