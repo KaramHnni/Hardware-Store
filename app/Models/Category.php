80042f92a54ac7d85de03eec6870c7cf31dab6eb
Henni Karam <karam.tenes@gmail.com>
2018-11-09 16:29:28 +0100

    Add status attribute to users posts and categories tables

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,24 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+    
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+   
 }
