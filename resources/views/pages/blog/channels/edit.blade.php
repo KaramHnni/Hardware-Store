5a29c8c190323ec33961029a0599c1b0afcfe925
Henni Karam <karam.tenes@gmail.com>
2018-11-14 14:46:57 +0100

    Fix Issues

--- resources/views/pages/blog/channels/edit.blade.php
+++ resources/views/pages/blog/channels/edit.blade.php
@@ -13,12 +13,11 @@
 
 <div class="w-4/5 mx-auto my-12">
     <h1 class="text-center font-bold text-2xl mb-8 ">Edit Your channel</h1>
-
-    <form method="POST" action = "{{route('blog.channel.edit')}}" enctype="multipart/form-data">
+    <form method="POST" action = "{{route('blog.channel.edit',['slug' =>$channel->slug ])}}" enctype="multipart/form-data">
         @csrf
 
     <input type="text" value="{{$channel->name}}" placeholder="Channel's name" name="name" class="px-2 py-4 w-full  my-8 block border-b border-solid border-grey-light text-2xl text-grey-dark font-bold ">
-        <input type="submit" value="Create Your Channel" class="bg-grey-dark text-white font-semibold inline-block px-4 py-4">
+        <input type="submit" value="Edit Your Channel" class="bg-grey-dark text-white font-semibold inline-block px-4 py-4">
     </form>
         
 
