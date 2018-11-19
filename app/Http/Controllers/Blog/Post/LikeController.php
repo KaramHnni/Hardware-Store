4463aee21646852c5e49d8daee35f4526c0d16ff
Henni Karam <karam.tenes@gmail.com>
2018-11-19 21:55:51 +0100

    finish liking system

---
+++ app/Http/Controllers/Blog/Post/LikeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Post;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LikeController extends Controller
+{
+    public function like($slug){
+
+        $post = Post::fetchBySlug($slug);
+
+        $post->users()->attach(auth()->user()->id);
+
+        return redirect()->back()->with('liked',true);
+    }
+
+    public function dislike($slug){
+
+        $post = Post::fetchBySlug($slug);
+
+        $post->users()->detach(auth()->user()->id);
+
+        return redirect()->back()->with('unfollowed',true);
+    }
+}
--- resources/views/pages/blog/posts/show.blade.php
+++ resources/views/pages/blog/posts/show.blade.php
@@ -15,5 +15,12 @@
         <h1 class="text-5xl font-bold mb-4">{{$post->title}}</h1>
         <p class="text-grey-dark" >Written on {{$post->channel->name}} By : {{$post->publisher->user->fullName}}</p>
         <p class="my-8 text-lg">{{$post->body}}</p>
+    @if( ! $post->users()->where('user_id',auth()->user()->id)->exists())
+        <a href="{{route('blog.post.like',$post->slug)}}" class="my-8 text-lg text-black">Like</a>
+    @else
+
+    <a href="{{route('blog.post.dislike',$post->slug)}}" class="my-8 text-lg text-blue font-bold">Liked</a>
+
+    @endif
 </div>
 @endsection
