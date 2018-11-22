8db9049adce5934af047efe5d2282b0b515e1e2b
Henni Karam <karam.tenes@gmail.com>
2018-11-22 17:18:20 +0100

    Add delete post method

--- app/Http/Controllers/Blog/Post/DeleteController.php
+++ app/Http/Controllers/Blog/Post/DeleteController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Blog\Post;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DeleteController extends Controller
 {
-    //
+    public function delete($slug){
+
+        $post = Post::fetchBySlug($slug);
+        $post = $post->delete();
+
+        return redirect(route('blog.show'));
+
+    }
 }
