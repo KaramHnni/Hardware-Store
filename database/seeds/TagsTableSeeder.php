69ffdeabd3c532129df6bd78fbd99aff155f824b
Henni Karam <karam.tenes@gmail.com>
2018-11-30 16:56:58 +0100

    create tags table seeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tags')->insert([
+            'name' => 'Hardware',
+            'slug' => 'hardware',
+            'created_at' => now(),
+            'status' => '1'
+        ]); 
+        DB::table('tags')->insert([
+            'name' => 'MD',
+            'slug' => 'md',
+            'created_at' => now(),
+            'status' => '1'
+        ]); DB::table('tags')->insert([
+            'name' => 'Surgery',
+            'slug' => 'Orthopedic Surgery',
+            'created_at' => now(),
+            'status' => '1'
+        ]);  
+    }
+}
