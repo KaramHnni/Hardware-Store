d8c1a917a7cf7be8dd0403b313c90d84c2bbd65a
Henni Karam <karam.tenes@gmail.com>
2018-11-30 15:27:36 +0100

    Fix article layout

--- resources/views/components/blog/article-card.blade.php
+++ resources/views/components/blog/article-card.blade.php
@@ -1,13 +1,12 @@
-<article style="width:400px; height:560px;" class=" shadow border border-solid border-grey-light my-8">
-        <div style="height:100px" class="flex justfy-between items-center">
-        <p>{{$article_category}}</p>
+<article style="width:400px; height:650px;" class=" shadow border border-solid border-grey-light my-8">
+        <div style="height:50px" class="px-4 flex justfy-between items-center">
+        <p class="font-bold ">{{$article_category}}</p>
         <p>{{$article_date}}</p>
-
         </div>
         <img style="height:300px;" class="block w-full bg-grey-light" src="{{$link_image}}">
         <div>
-        <p style="height:30px;" class=" my-12 mx-4 text-2xl font-bold " >{{$article_title}}</p>
-    <p class=" my-12 mx-4 text-xl" >{{$article_body_part}}...</p>
+        <p style="height:25px;" class=" my-12 mx-4 text-xl font-bold " >{{$article_title}}</p>
+        <p style="height:50px;" class=" mx-4 text-xl" >{{$article_body_part}}...</p>
         <a href="/blog/Posts/{{$article_slug}}"class="block text-xl my-12 mx-4 ">Continue Reading > </a>
         </div>
     </article>
