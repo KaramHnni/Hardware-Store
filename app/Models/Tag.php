da308ac0b516ae40651504a9552c0eb6f6e6b4a5
Henni Karam <karam.tenes@gmail.com>
2018-11-30 15:52:47 +0100

    Add tag essential methods

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tags';
+    
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    public function getStatusNameAttribute(){
+        if($this->status == false){
+            return " Inactive ";
+        }
+        if($this->status == true){
+            return " Active " ; 
+        }
+    }
+
+    public static function active(){
+       
+        return self::where('status',1);
+    }
+ 
+    public function setActive(){
+        $this->status = 1;
+        $this->updated_at = now();
+        $this->save();
+    }
+ 
+    public function setInactive(){
+        $this->status = 0;
+        $this->updated_at = now();
+        $this->save();
+    }
+
+    public static function fetchBySlug($slug){
+        return self::where('slug',$slug)->first();
+    }
+
 }
